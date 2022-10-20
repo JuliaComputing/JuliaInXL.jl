396a4fc3c9baf8ce0e4b1ae13b28921719d6edb5
Harsha Byadarahalli Mahesh <bmharsha@users.noreply.github.com>
2022-10-20 20:04:57 +0530

    Testing the MIT version of EXE

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using Pkg
 using Pkg.Artifacts
 
 if Sys.iswindows()
-    juliainxl_exe_path = joinpath(artifact"JuliaInXL","JuliaPro_JuliaInXL_v1.1.2.1.exe")    
+    juliainxl_exe_path = joinpath(artifact"JuliaInXL","JuliaPro_JuliaInXL_v1.7.3.1.exe")    
 	run(`cmd /c $(juliainxl_exe_path) $(Sys.BINDIR)`)
 else
 	@error("Your operating system is not compatible with JuliaInXL. This package works only on Windows")
