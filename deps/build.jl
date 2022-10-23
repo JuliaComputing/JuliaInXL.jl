926f04ac2b2d9d03347014a6b9b3b44808593f7f
Avik Sengupta <avik@sengupta.net>
2022-10-23 18:23:52 +0100

    Change version to 1.2.0

--- deps/build.jl
+++ deps/build.jl
@@ -5,5 +5,5 @@ if Sys.iswindows()
     juliainxl_exe_path = joinpath(artifact"JuliaInXL","JuliaPro_JuliaInXL_v1.7.3.1.exe")    
 	run(`cmd /c $(juliainxl_exe_path) $(Sys.BINDIR)`)
 else
-	@error("Your operating system is not compatible with JuliaInXL. This package works only on Windows")
+	@warn("Your operating system is not compatible with JuliaInXL. This package works only on Windows")
 end
